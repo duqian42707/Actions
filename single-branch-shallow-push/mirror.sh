8d57ecd99cd59d84ad7a52c28adc5948b255eaab
duqian <duqian42707@163.com>
2023-02-17 13:51:06 +0800

    update action

--- single-branch-shallow-push/mirror.sh
+++ single-branch-shallow-push/mirror.sh
@@ -6,11 +6,12 @@ ls -alh
 
 export GIT_SSH_COMMAND="ssh -v -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -l $INPUT_SSH_USERNAME"
 
+git config --global --add safe.directory /github/workspace
 branch_name=$(git symbolic-ref --short -q HEAD)
 message=$(git log --format=%B -n 1)
 git checkout --orphan e3a3bcbf11db4c5e9199e81e21e3d7cf
 git add -A
-git commit -m '$message'
+git commit -m "$message"
 git remote add mirror "$INPUT_TARGET_REPO_URL"
 git push --force mirror "$branch_name"
 
