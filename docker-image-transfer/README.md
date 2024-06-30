381ad9b3125a636ee1034c122716c9eb8413d5a4
duqian <duqian42707@163.com>
2024-06-30 21:50:34 +0800

    action: docker-image-transfer

---
+++ docker-image-transfer/README.md
@@ -0,0 +1,27 @@
+# gcr.io images Sync
+
+## Usage
+
+```yaml
+name: docker image sync
+on:
+  workflow_dispatch:
+    inputs:
+      images:
+        description: "docker image"
+        required: true
+        default: "nginx:latest"
+jobs:
+  k8s-image-sync:
+    name: gcr.io image sync
+    runs-on: ubuntu-latest
+    steps:
+    - name: sync
+      uses: duqian42707/actions/gcr-io-image-sync@main
+      with:
+        images: ${{ github.event.inputs.images }}
+        username: ${{ secrets.DOCKER_USERNAME }}
+        password: ${{ secrets.DOCKER_PASSWORD }}
+        registry: swr.cn-north-4.myhuaweicloud.com
+        namespace: dqv5
+```
---
+++ docker-image-transfer/entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "-------------[image list]-------------"
+echo $INPUT_IMAGES
+
+docker login -u ${INPUT_USERNAME} -p ${INPUT_PASSWORD} ${INPUT_REGISTRY}
+
+for line in $INPUT_IMAGES
+do
+    old_image=${line}
+    IFS='/'
+    arr=(${line})
+    length=(${#arr[*]})
+    if [ $length -gt 2 ];then
+      new_image="${INPUT_NAMESPACE}/${arr[1]}.${arr[2]}"
+    else
+      new_image="${INPUT_NAMESPACE}/${arr[1]}"
+    fi
+    echo "-------------[docker pull ${old_image}]-------------"
+    docker pull "${old_image}"
+    echo "-------------[docker tag ${old_image} ${new_image}]-------------"
+    docker tag "${old_image}" "${new_image}"
+    echo "-------------[docker push ${new_image}]-------------"
+    docker push "${new_image}"
+done
+
+
