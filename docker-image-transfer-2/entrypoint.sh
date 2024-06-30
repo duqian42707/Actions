b9ac3ff52ad357c9ead00a63fddf7a24f7353bc4
duqian <duqian42707@163.com>
2024-06-30 22:37:48 +0800

    action: docker-image-transfer-2

--- docker-image-transfer-2/entrypoint.sh
+++ docker-image-transfer-2/entrypoint.sh
@@ -3,7 +3,7 @@
 echo "-------------[START]-------------"
 echo $INPUT_REGISTRY_SECRET > /registry-secret.yaml
 echo $INPUT_TRANSFER_RULE > /transfer-rule.yaml
-/image-transfer --securityFile=./registry-secret.yaml --ruleFile=./transfer-rule.yaml
+/image-transfer --securityFile=/registry-secret.yaml --ruleFile=/transfer-rule.yaml
 
 
 
