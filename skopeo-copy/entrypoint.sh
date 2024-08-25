07a5414c55e541cf0c7d3f8d350681a979da7168
duqian <duqian42707@163.com>
2024-08-25 18:41:28 +0800

    action: skopeo-copy

--- skopeo-copy/entrypoint.sh
+++ skopeo-copy/entrypoint.sh
@@ -3,6 +3,7 @@
 echo "-------------[image list]-------------"
 echo $INPUT_IMAGES
 
+echo "-------------[skopeo login]-------------"
 skopeo login -u ${INPUT_USERNAME} -p ${INPUT_PASSWORD} ${INPUT_REGISTRY}
 
 for line in $INPUT_IMAGES
@@ -12,6 +13,7 @@ do
     new_image="${INPUT_REGISTRY}/${INPUT_NAMESPACE}/${img}"
     echo "-------------[old_image: ${old_image}]-------------"
     echo "-------------[new_image: ${new_image}]-------------"
+    echo "-------------[skopeo copy --multi-arch all docker://${old_image} docker://${new_image}]-------------"
     skopeo copy --multi-arch all docker://$old_image docker://$new_image
 done
 
