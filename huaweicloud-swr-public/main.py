1654fd6f0af25e38b031fa8aff09d6e4a95bf56b
duqian <duqian42707@163.com>
2024-10-27 00:06:56 +0800

    action: huaweicloud-swr-public

--- huaweicloud-swr-public/main.py
+++ huaweicloud-swr-public/main.py
@@ -6,27 +6,17 @@ from huaweicloudsdkswr.v2.region.swr_region import SwrRegion
 from huaweicloudsdkcore.exceptions import exceptions
 from huaweicloudsdkswr.v2 import *
 
-if __name__ == "__main__":
-    # The AK and SK used for authentication are hard-coded or stored in plaintext, which has great security risks. It is recommended that the AK and SK be stored in ciphertext in configuration files or environment variables and decrypted during use to ensure security.
-    # In this example, AK and SK are stored in environment variables for authentication. Before running this example, set environment variables CLOUD_SDK_AK and CLOUD_SDK_SK in the local environment
-    ak = os.getenv("HUAWEI_CLOUD_SDK_AK")
-    sk = os.getenv("HUAWEI_CLOUD_SDK_SK")
-    region = os.getenv("HUAWEI_CLOUD_SWR_REGION")
-    namespace = os.getenv("HUAWEI_CLOUD_SWR_NAMESPACE")
-    repository = os.getenv("HUAWEI_CLOUD_SWR_REPOSITORY")
-    repository = repository.replace("/", "$")
+ak = os.getenv("HUAWEI_CLOUD_SDK_AK")
+sk = os.getenv("HUAWEI_CLOUD_SDK_SK")
+region = os.getenv("HUAWEI_CLOUD_SWR_REGION")
+namespace = os.getenv("HUAWEI_CLOUD_SWR_NAMESPACE")
+repos = os.getenv("HUAWEI_CLOUD_SWR_REPOS")
 
-    credentials = BasicCredentials(ak, sk)
-
-    client = SwrClient.new_builder() \
-        .with_credentials(credentials) \
-        .with_region(SwrRegion.value_of(region)) \
-        .build()
-
+def send_request(repository):
     try:
         request = UpdateRepoRequest(namespace, repository)
         request.body = UpdateRepoRequestBody(
-            is_public=True
+            is_public=False
         )
         response = client.update_repo(request)
         print(response)
@@ -35,3 +25,14 @@ if __name__ == "__main__":
         print(e.request_id)
         print(e.error_code)
         print(e.error_msg)
+
+
+if __name__ == "__main__":
+    credentials = BasicCredentials(ak, sk)
+    client = SwrClient.new_builder() \
+        .with_credentials(credentials) \
+        .with_region(SwrRegion.value_of(region)) \
+        .build()
+    strs = repos.split(',')
+    for repo in strs:
+        send_request(repository=repo)
--- skopeo-copy/entrypoint.sh
+++ skopeo-copy/entrypoint.sh
@@ -6,6 +6,7 @@ echo $INPUT_IMAGES
 echo "-------------[skopeo login]-------------"
 skopeo login -u ${INPUT_USERNAME} -p ${INPUT_PASSWORD} ${INPUT_REGISTRY}
 
+REPOSITORY_NAMES=""
 for line in $INPUT_IMAGES
 do
     old_image=${line}
@@ -14,10 +15,14 @@ do
     echo "-------------[old_image: ${old_image}]-------------"
     echo "-------------[new_image: ${new_image}]-------------"
     echo "-------------[skopeo copy --multi-arch all docker://${old_image} docker://${new_image}]-------------"
+    REPOSITORY_NAMES="${REPOSITORY_NAMES},${img%:*}"
     skopeo copy --multi-arch all docker://$old_image docker://$new_image
 done
 
-
+REPOSITORY_NAMES=${REPOSITORY_NAMES:1}
+# 设置步骤的输出参数
+#echo "{name}={value}" >> "$GITHUB_OUTPUT"
+echo "REPOSITORY_NAMES=$REPOSITORY_NAMES" >> "$GITHUB_OUTPUT"
 
 
 
